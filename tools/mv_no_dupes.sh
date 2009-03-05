fb7e2f47c75f3b7acf3699d5dd4f10b173309aed
troy <troy@c0f1bf54-08b6-4aeb-9e84-c69c557caba5>
2009-03-05 01:10:48 +0000

    Added breweries and fixed some bugs

--- tools/mv_no_dupes.sh
+++ tools/mv_no_dupes.sh
@@ -6,10 +6,28 @@ DEST=$2;
 for D in `ls $SRC/`; do
 	
 	for F in `ls $SRC/$D/`; do
-		# echo "Testing $F";
 		if [ -f $DEST/$D/$F ]; then 
-			echo "Dupes: $SRC/$D/$F $DEST/$D/$F"; 
+			# File is there, see if it's the same
+			if ! diff $SRC/$D/$F $DEST/$D/$F > /dev/null; then
+				echo "Different: $SRC/$D/$F $DEST/$D/$F"; 
+			else
+				# File is identical, delete this one
+				rm $SRC/$D/$F
+			fi
+		else
+			if [ ! -d $DEST/$D ]; then
+				mkdir $DEST/$D;
+			fi
+			mv -i $SRC/$D/$F $DEST/$D/$F
 		fi
 	done
 
+done
+
+# Remove any empty dirs in $SRC
+find $SRC -type d | while read D; do
+	COUNT=`ls $D | wc -l`;
+	if [ $COUNT -eq 0 ]; then
+		rmdir $D
+	fi
 done
