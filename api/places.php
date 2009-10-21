4a5e7fbda53da7fc54c61ad3dd254a8a56295bca
troy <troy@c0f1bf54-08b6-4aeb-9e84-c69c557caba5>
2009-10-21 00:16:29 +0000

    New Places list API

---
+++ api/places.php
@@ -0,0 +1,23 @@
+<?php
+require_once('beercrush/oak.class.php');
+
+$oak=new OAK;
+$places=new stdClass;
+$oak->get_view('place/all',$places);
+// var_dump($places);exit;
+
+$doc=array();
+foreach ($places->rows as $row)
+{
+	$letter=strtoupper(substr($row->key,0,1));
+	if (!ctype_alpha($letter))
+		$letter='#';
+	$doc[$letter][]=array(
+		"id" => $row->id,
+		"name" => $row->key,
+	);
+}
+
+print json_encode($doc);
+
+?>
