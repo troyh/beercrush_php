4ae703586b46eae5e993e73c74bbaa724cd9a7f7
troy <troy@c0f1bf54-08b6-4aeb-9e84-c69c557caba5>
2009-10-24 17:23:59 +0000

    Fixed Unicode character problem

--- api/breweries.php
+++ api/breweries.php
@@ -1,6 +1,10 @@
 <?php
 require_once('beercrush/oak.class.php');
 
+header('Content-type: text/javascript; charset=utf-8');
+
+mb_internal_encoding("UTF-8");
+
 $oak=new OAK;
 $breweries=new stdClass;
 $oak->get_view('brewery/all',$breweries);
@@ -9,7 +13,8 @@ $oak->get_view('brewery/all',$breweries);
 $doc=array();
 foreach ($breweries->rows as $row)
 {
-	$letter=strtoupper(substr($row->key,0,1));
+	$c=mb_substr($row->key,0,1);
+	$letter=mb_strtoupper(remove_diacritics($c));
 	if (!ctype_alpha($letter))
 		$letter='#';
 	$doc[$letter][]=array(
@@ -17,7 +22,23 @@ foreach ($breweries->rows as $row)
 		"name" => $row->key,
 	);
 }
-
+// exit;
+ksort($doc);
 print json_encode($doc);
+
+function remove_diacritics($string) {
+    $table = array(
+        'Š'=>'S', 'š'=>'s', 'Đ'=>'Dj', 'đ'=>'dj', 'Ž'=>'Z', 'ž'=>'z', 'Č'=>'C', 'č'=>'c', 'Ć'=>'C', 'ć'=>'c',
+        'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
+        'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O',
+        'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U', 'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss',
+        'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c', 'è'=>'e', 'é'=>'e',
+        'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o',
+        'ô'=>'o', 'õ'=>'o', 'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'ý'=>'y', 'þ'=>'b',
+        'ÿ'=>'y', 'Ŕ'=>'R', 'ŕ'=>'r', 'Ś'=>'S', 'Ą'=>'A'
+    );
+    
+    return strtr($string, $table);
+}
 
 ?>
