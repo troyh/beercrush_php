008ff96c45005f8f8dc818702743659b5eb7901c
troy <troy@c0f1bf54-08b6-4aeb-9e84-c69c557caba5>
2009-11-22 17:49:50 +0000

    changed content-type to application/json

--- api/beer/view.php
+++ api/beer/view.php
@@ -1,12 +1,20 @@
 <?php
 require_once('beercrush/oak.class.php');
+// header('Content-Type: text/plain');
+// print_r($_SERVER);exit;
+if (empty($_GET['beer_id']))
+{
+	header('HTTP/1.0 404 No beer_id');
+	print "No beer_id";
+	exit;
+}
 
 $oak=new OAK;
 $beerdoc=new OAKDocument('');
 $oak->get_document($_GET['beer_id'],$beerdoc);
 unset($beerdoc->_id);
 unset($beerdoc->_rev);
-header('Content-Type: text/javascript; charset=utf-8');
+header('Content-Type: application/json; charset=utf-8');
 print json_encode($beerdoc);
 
 ?>
