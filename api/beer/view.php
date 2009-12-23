c8d29f3a2f34d98930441aac1950af83121d638f
troy <troy@c0f1bf54-08b6-4aeb-9e84-c69c557caba5>
2009-12-23 18:41:26 +0000

    Adds review_summary data

--- api/beer/view.php
+++ api/beer/view.php
@@ -9,6 +9,16 @@ if (empty($_GET['beer_id']))
 	exit;
 }
 
+function tally_sum($row,$name,&$averages)
+{
+	if (isset($row->value->$name))
+	{
+		$averages[$name]+=$row->value->$name;
+		$averages["{$name}_count"]++;
+	}
+	
+}
+
 $oak=new OAK;
 $beerdoc=new OAKDocument('');
 $oak->get_document($_GET['beer_id'],$beerdoc);
@@ -16,18 +26,52 @@ $beerdoc->id=$beerdoc->_id;
 unset($beerdoc->_id);
 unset($beerdoc->_rev);
 
+/*
+	Compute review_summary data
+*/
 $reviews=new stdClass;
 $oak->get_view('beer_reviews/for_beer?key=%22'.$_GET['beer_id'].'%22',&$reviews);
 
-$total_rating=0;
+$averages=array();
+$flavors=array();
+
 foreach ($reviews->rows as $row)
 {
-	$total_rating+=$row->value->rating;
+	// rating, srm, body, balance & aftertaste are all numeric values so we average them
+	tally_sum($row,'rating',$averages);
+	tally_sum($row,'srm',$averages);
+	tally_sum($row,'body',$averages);
+	tally_sum($row,'balance',$averages);
+	tally_sum($row,'aftertaste',$averages);
+	
+	// flavors is an array of values. We want the most common ones so collect them all.
+	if (!empty($row->value->flavors))
+	{
+		foreach ($row->value->flavors as $flavor)
+		{
+			if (!isset($flavors[$flavor]))
+				$flavors[$flavor]=0;
+			$flavors[$flavor]++;
+		}
+	}
 }
 
-$beerdoc->rating_count=count($reviews->rows);
-if ($beerdoc->rating_count)
-	$beerdoc->rating_avg=$total_rating / $beerdoc->rating_count;
+$beerdoc->review_summary=new stdClass;
+$beerdoc->review_summary->total=$averages['rating_count'];
+if ($averages['rating_count'])
+	$beerdoc->review_summary->avg=$averages['rating'] / $averages['rating_count'];
+if ($averages['srm_count'])
+	$beerdoc->review_summary->srm_avg=$averages['srm'] / $averages['srm_count'];
+if ($averages['body_count'])
+	$beerdoc->review_summary->body_avg=$averages['body'] / $averages['body_count'];
+if ($averages['balance_count'])
+	$beerdoc->review_summary->balance_avg=$averages['balance'] / $averages['balance_count'];
+if ($averages['aftertaste_count'])
+	$beerdoc->review_summary->aftertaste_avg=$averages['aftertaste'] / $averages['aftertaste_count'];
+
+// Get the most-common flavors
+if (arsort($flavors)===true)
+	$beerdoc->review_summary->flavors=array_slice(array_keys($flavors),0,5);
 
 header('Content-Type: application/json; charset=utf-8');
 print json_encode($beerdoc);
