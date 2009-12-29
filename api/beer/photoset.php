5ee7b25b772cba12bf18c2cfc5893f4b94bcd0af
troy <troy@c0f1bf54-08b6-4aeb-9e84-c69c557caba5>
2009-12-29 01:15:37 +0000

    New page

---
+++ api/beer/photoset.php
@@ -0,0 +1,22 @@
+<?php
+// header("Cache-Control: no-cache");
+require_once('beercrush/oak.class.php');
+
+if (empty($_GET['beer_id']))
+{
+	header('HTTP/1.0 404 No beer_id');
+	print "No beer_id";
+	exit;
+}
+
+$oak=new OAK;
+$photoset=new OAKDocument('');
+$oak->get_document('photoset:'.$_GET['beer_id'],$photoset);
+$photoset->id=$photoset->_id;
+unset($photoset->_id);
+unset($photoset->_rev);
+
+header('Content-Type: application/json; charset=utf-8');
+print json_encode($photoset);
+
+?>
