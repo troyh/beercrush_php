3d6b1679e70698a5b58a0d6bdb9f883f91fc573e
troy <troy@c0f1bf54-08b6-4aeb-9e84-c69c557caba5>
2010-02-05 23:41:46 +0000

    Creates web directory, if necessary

--- install.sh
+++ install.sh
@@ -2,6 +2,23 @@
 
 . ./config.sh;
 
-if [ ! -d $WWW_DIR ]; then
-	mkdir $WWW_DIR;
+if [ `tools/iamservertype -q php-cgi` -o `tools/iamservertype -q web` ]; then
+
+	if [ ! -d $WWW_DIR ]; then
+		echo "Creating $WWW_DIR";
+		sudo mkdir $WWW_DIR;
+	fi
+	
+	if [ "$(ls -ld $WWW_DIR | awk '{print $3" "$4}')" != "www-data www-data" ]; then
+		echo "Setting permissions on $WWW_DIR";
+		sudo chown www-data.www-data $WWW_DIR;
+		sudo chmod g+w $WWW_DIR;
+	fi
+
+	if tools/iamservertype -q php-cgi; then
+		if [ ! -d $WWW_DIR/uploads ]; then
+			mkdir $WWW_DIR/uploads;
+		fi
+	fi
+	
 fi
