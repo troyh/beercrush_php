83694298484d235f1d35a2c86971c77c2b9a2f94
troy <troy@c0f1bf54-08b6-4aeb-9e84-c69c557caba5>
2010-02-15 18:45:30 +0000

    make uses yui-compressor to minify the js files and make install installs the minified versions

--- js/install.sh
+++ js/install.sh
@@ -7,5 +7,7 @@ if ../tools/iamservertype -q web; then
 		echo "Making $WWW_DIR/js";
 		mkdir $WWW_DIR/js;
 	fi
-	rsync --recursive --delete *.js $WWW_DIR/js/;
+
+	rsync --recursive --delete mini/*.js $WWW_DIR/js/;
+	
 fi
