6288666992337e14c37a3187362173f3899f3f20
troy <troy@c0f1bf54-08b6-4aeb-9e84-c69c557caba5>
2010-03-17 19:58:51 +0000

    fixed bad path in Solr config

--- conf/solr/install.sh
+++ conf/solr/install.sh
@@ -7,19 +7,19 @@
 
 if ../../tools/iamservertype -q solr; then
 
-	if ! diff -u conf/solr/solrconfig.xml /etc/solr/conf/solrconfig.xml > /dev/null; then
+	if ! diff -u solrconfig.xml /etc/solr/conf/solrconfig.xml > /dev/null; then
 		echo "**************************************************";
 		echo "ERROR: Solr config (/etc/solr/conf/solrconfig.xml) is incorrect:";
 		echo "**************************************************";
-		diff -u conf/solr/solrconfig.xml /etc/solr/conf/solrconfig.xml;
+		diff -u solrconfig.xml /etc/solr/conf/solrconfig.xml;
 		exit;
 	fi
 
-	if ! diff -u conf/solr/schema.xml /etc/solr/conf/schema.xml > /dev/null; then
+	if ! diff -u schema.xml /etc/solr/conf/schema.xml > /dev/null; then
 		echo "**************************************************";
 		echo "ERROR: Solr schema (/etc/solr/conf/schema.xml) is incorrect:";
 		echo "**************************************************";
-		diff -u conf/solr/schema.xml /etc/solr/conf/schema.xml;
+		diff -u schema.xml /etc/solr/conf/schema.xml;
 		exit;
 	fi
 
