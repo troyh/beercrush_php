92117743a456302822b8f176f9b1267bd65b739d
troy <troy@c0f1bf54-08b6-4aeb-9e84-c69c557caba5>
2010-03-17 22:31:46 +0000

    Added test for NGiNX version

--- conf/nginx/check.sh
+++ conf/nginx/check.sh
@@ -33,3 +33,25 @@ if ../../tools/iamservertype -q solr-proxy; then
 	fi
 
 fi
+
+if ../../tools/iamservertype -q couchdb-proxy || ../../tools/iamservertype -q solr-proxy || ../../tools/iamservertype -q web; then
+
+	NGINX_VER=`/usr/sbin/nginx -v 2>&1`; # nginx -v writes to stderr for some reason
+	if [ "$NGINX_VER" != "nginx version: nginx/0.8.26" ]; then
+		cat - <<EOF
+	You must use NGiNX 0.8.26. You're using $NGINX_VER.
+	
+	To build it:
+
+	tar xvzf nginx-0.8.26.tar.gz
+	tar xvzf ngx_cache_purge.tar.gz
+	cd nginx-0.8.26/
+	./configure --prefix=/usr  --sbin-path=/usr/sbin/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --pid-path=/var/run/nginx.pid --user=www-data --group=www-data --add-module=../ngx_cache_purge/
+	make
+	sudo make install
+
+EOF
+
+	fi
+
+fi
