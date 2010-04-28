f19208b76a774cea17d43eb475bda27695777ac1
troy <troy@c0f1bf54-08b6-4aeb-9e84-c69c557caba5>
2010-04-28 18:16:58 +0000

    fixed make check

--- make_recursively.sh
+++ make_recursively.sh
@@ -16,8 +16,8 @@ for MAKEFILE in $(find . -mindepth 2 -name Makefile -type f |sort| grep -v -e '/
 	if [[ $P2 =~ ^src/3rdparty/ ]]; then
 		continue;
 	fi
-	
+
 	echo "Make $TARGET ($MAKEFILE)";
 	make --silent -C $(dirname $MAKEFILE) $TARGET;
-	
+
 done
