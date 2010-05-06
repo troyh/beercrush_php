3cc6a980115ce78fb535d95cc04e8530b7ce6802
troy <troy@c0f1bf54-08b6-4aeb-9e84-c69c557caba5>
2010-05-06 23:27:54 +0000

    Fixed debugging edit in check.sh for Solr

--- conf/solr/check.sh
+++ conf/solr/check.sh
@@ -5,7 +5,7 @@ SOLR_URL=$(php -r '$cfg=json_decode(file_get_contents("/etc/BeerCrush/webapp.con
 
 php -r '$cfg=json_decode(file_get_contents("/etc/BeerCrush/setup.conf"));foreach ($cfg->servers->solr->servers as $node) {print $node."\n";}' |
 while read NODE; do
-	if ! curl --silent "http://x$NODE$SOLR_URL/select/?q=dogfish&wt=json" > /dev/null; then
+	if ! curl --silent "http://$NODE$SOLR_URL/select/?q=dogfish&wt=json" > /dev/null; then
 		echo "ERROR: Solr query failed on node $NODE";
 	fi
 done
