b61b1296e59958c95f3346e7040e3629f37538e4
troy <troy@c0f1bf54-08b6-4aeb-9e84-c69c557caba5>
2010-10-20 00:31:45 +0000

    fixes install errors

--- api/location/install.sh
+++ api/location/install.sh
@@ -1,5 +1,14 @@
 . ../../config.sh;
 
+if [ -z "$LOCALDATA_DIR" ]; then
+	echo "ERROR: LOCALDATA_DIR not specified.";
+	exit 1;
+fi
+
+if [ ! -d $LOCALDATA_DIR/meta/ ]; then
+	mkdir $LOCALDATA_DIR/meta;
+fi
+
 if [ ! -f $LOCALDATA_DIR/meta/latlonpairs.txt -o ! -f $LOCALDATA_DIR/meta/nearby_beer.txt ]; then
 	$BEERCRUSH_BIN_DIR/update_location_data -C /etc/BeerCrush/webapp.conf;
 fi
